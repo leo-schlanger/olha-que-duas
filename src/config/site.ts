418ac9f2fd61d84ca6b8ce61512b1ea8991a2c2f
leo-schlanger <leoschlanger@gmail.com>
2026-03-22 15:40:39 +0000

    chore: update radio tagline

--- src/config/site.ts
+++ src/config/site.ts
@@ -50,7 +50,7 @@ export const siteConfig = {
     // URL do stream AzuraCast
     streamUrl: "https://radio.olhaqueduas.com/listen/olha_que_duas/radio.mp3",
     isLive: true, // Rádio ativa
-    tagline: "A sua voz, 24 horas por dia",
+    tagline: "A sua companhia, 24 horas por dia",
   },
 
   // Parceiros
