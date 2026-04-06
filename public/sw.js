2e49cbbf35b6e8e8766a02e25dfbb72375cea6b0
leo-schlanger <leoschlanger@gmail.com>
2026-04-06 21:33:05 +0100

    fix: bump service worker cache version to v3

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Olha que Duas - Service Worker
 // PWA com cache inteligente
 
-const CACHE_NAME = 'olhaqueduas-v2';
+const CACHE_NAME = 'olhaqueduas-v3';
 const OFFLINE_URL = '/offline.html';
 
 // Assets para cache imediato (instalar)
